2d7d9bb3e4fee1fd6a904da26c93786b509f1f20
kleo4004 <zkioz4004@gmail.com>
2020-08-05 01:54:57 +0300

    feat:add XML request

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
 const searchForm = document.querySelector('#search-form');
+const movie = document.querySelector('#movies');
 
 function apiSearch(event) {
     event.preventDefault();
 
     const searchText = document.querySelector('.form-control').value;
-    const server = 'https://www.themoviedb.org/';
+    const server = 'https://api.themoviedb.org/3/search/multi?api_key=7c2ee62875ea6fb1aea5a9512a985137&language=ru&query=' + searchText;
+    requestApi('GET', server);
 }
 
 
 searchForm.addEventListener('submit', apiSearch);
 
-function requestApi(url) {
-    return url;
+function requestApi(method, url) {
+    const request = new XMLHttpRequest();
+    request.open(method, url);
+    request.send();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) return;
+        if (request.status !== 200) {
+            console.log("error: " + request.status);
+            return;
+        }
+        const output = JSON.parse(request.responseText);
+        let inner = '';
+        output.results.forEach(function(item) {
+            let nameItem = item.name || item.title;
+            inner += `<div class="col-5">${nameItem}</div>`;
+        });
+        movie.innerHTML = inner;
+        
+    });
 }
+
+// 7c2ee62875ea6fb1aea5a9512a985137 
