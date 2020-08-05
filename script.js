796627b0b000883b9dade266a7385c329eb2f15c
kleo4004 <zkioz4004@gmail.com>
2020-08-05 22:45:06 +0300

    feat:add promise

--- script.js
+++ script.js
@@ -6,32 +6,61 @@ function apiSearch(event) {
 
     const searchText = document.querySelector('.form-control').value;
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=7c2ee62875ea6fb1aea5a9512a985137&language=ru&query=' + searchText;
-    requestApi('GET', server);
-}
-
-
-searchForm.addEventListener('submit', apiSearch);
-
-function requestApi(method, url) {
-    const request = new XMLHttpRequest();
-    request.open(method, url);
-    request.send();
-
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState !== 4) return;
-        if (request.status !== 200) {
-            console.log("error: " + request.status);
-            return;
-        }
-        const output = JSON.parse(request.responseText);
+    movie.innerHTML = 'ЗАГРУЗКА...';
+    requestApi(server)
+    .then(function(result) { //result  это request.response
+        const output = JSON.parse(result);
         let inner = '';
         output.results.forEach(function(item) {
             let nameItem = item.name || item.title;
             inner += `<div class="col-5">${nameItem}</div>`;
         });
         movie.innerHTML = inner;
-        
+    })
+    .catch(function(reason) {
+        movie.innerHTML = 'УПС... что-то пошло не так...';
+        console.log("error: " + reason.status);
     });
 }
+
+
+searchForm.addEventListener('submit', apiSearch);
+
+function requestApi(url) {
+    return new Promise(function(resolve, reject) {
+        const request = new XMLHttpRequest();
+        request.open('GET', url);
+        //ошибка 404 попадет в load, а не error - поэтому !== 200
+        request.addEventListener('load', function() {
+            if (request.status !== 200) {
+                reject({status: request.status});
+                return;
+            }
+
+            resolve(request.response);
+        });
+        request.addEventListener('error', function() {
+            reject({status: request.status});
+        });
+        request.send();
+    });
+    
+}   
+    
+
+//     request.addEventListener('readystatechange', () => {
+//         if (request.readyState !== 4) {
+//            
+//             return;
+//         } 
+//         if (request.status !== 200) {
+            
+//            
+//             return;
+//         }
+        
+        
+//     });
+// }
 
 // 7c2ee62875ea6fb1aea5a9512a985137 
